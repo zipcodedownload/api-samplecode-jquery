5f5ec604a4401ca1b93b8bfdf29ad6b0e4260139
mukulvtilak <tilak.mukul@hotmail.com>
2018-06-05 18:17:02 +0530

    code indent and drop down functionality

--- scripts/index-script.js
+++ scripts/index-script.js
@@ -1,5 +1,5 @@
 let context = this;
-$('#zipCode').on('keyup',function () {
+$('#zipCode').on('keyup', function () {
     // check if input is zip code or a postal code
     // makes call to assembleURL() and sends parameters
     let val = document.getElementById('zipCode').value
@@ -14,17 +14,17 @@ $('#zipCode').on('keyup',function () {
     ];
     event.preventDefault();
     let canRegEx = /^[ABCEGHJKLMNPRSTVXY]\d[ABCEGHJKLMNPRSTVWXYZ]( )?/i,
-            usRegEx = /^\d{5}(-\d{4})?$/i;
-    if(val.match(canRegEx) && val.length === 6) {
+        usRegEx = /^\d{5}(-\d{4})?$/i;
+    if (val.match(canRegEx) && val.length === 6) {
         model.country = 'ca';
         model.postalcode = val;
-        $('#zipCodeValidation').css('display','none');
+        $('#zipCodeValidation').css('display', 'none');
         context.assembleURL(model, 'Filter');
     }
-    else if(val.match(usRegEx) && val.length === 5) {
+    else if (val.match(usRegEx) && val.length === 5) {
         model.country = 'us5';
         model.postalcode = val;
-        $('#zipCodeValidation').css('display','none');
+        $('#zipCodeValidation').css('display', 'none');
         context.assembleURL(model, 'Filter');
     }
     else {
@@ -35,24 +35,37 @@ $('#zipCode').on('keyup',function () {
 
 function afterGettingData(data) {
     // this will populate relevant form fields
-    if(data.length === 0) {
+    console.log(data);
+    if (data.length === 0) {
         handleValidations();
         return;
     }
 
     var dummy;
-    if(data.length != undefined)
-        data.forEach(item => {
-            dummy = item.city_type === 'D' ? item : {};
-        });
+    if (data.length != undefined)
+    for(var i = 0; i < data.length; i++) {
+        if(data[i].city_type === 'D') {
+            dummy = data[i];
+            break;
+        }
+    }
     else
         dummy = data;
-    
+    console.log(dummy);
     $('#cityName').val(dummy.city_name);
     $('#provinceName').val(dummy.province);
+
+    var selectHTML = '';
+    for(i=0; i<data.length; i=i+1){
+        selectHTML+= "<option value='"+data[i].city_name+"'>"+data[i].city_name+"</option>";
+    }
+    document.getElementById('selectCityName').innerHTML = selectHTML;
 }
 
 function handleValidations() {
     $('#zipCodeValidation').text('Incorrect zip/postal code')
-    $('#zipCodeValidation').css('display','inline-block');
+    $('#zipCodeValidation').css('display', 'inline-block');
+    $('#cityName').val('');
+    $('#provinceName').val('');
+    document.getElementById('selectCityName').innerHTML = "<option value='NULL'>Select</option>";
 }
